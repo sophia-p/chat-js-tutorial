3e79adef340ff3741fc3dbbbc7f9058bba1bfa6f
Sophia Pignatelli <pignatelli@fordham.edu>
2017-05-17 17:44:33 -0400

    finish tutorial

--- index.js
+++ index.js
@@ -15,3 +15,10 @@ app.use(express.static(__dirname + '/public'));
 
 var io = require('socket.io').listen(app.listen(port))
 console.log("Listening on port " + port)
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+});
